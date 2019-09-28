159b5ab501347bf4ad0ba7a846294cc09cbf1fc0
ciprianoe <ciprianoescorche@gmail.com>
2019-09-28 01:17:18 -0400

    VueJS

--- vue.js
+++ vue.js
@@ -82,7 +82,7 @@ const vm = new Vue({
 
 // video 5 eventos
 
-const vm = new Vue({
+/* const vm = new Vue({
     el: 'main',
     data:{
         nuevaTarea: null,
@@ -108,6 +108,98 @@ const vm = new Vue({
 
         }
     }
+
+
+
+}); */
+
+// video 6
+
+/* const vm = new Vue({
+        el: 'main',
+        nuevaTarea: null,
+        data:{
+                mensaje: 'Hola VueJS',
+
+            tareas:[
+            {nombre:'Aprender VueJS', prioridad:true, antiguedad:10},
+            {nombre:'Tomar un descanso', prioridad:false, antiguedad:15},
+            {nombre:'Hacer Ejercicios', prioridad:true, antiguedad:20},
+            {nombre:'Pasar tiempo con la familia',prioridad:true, antiguedad:30},
+        ],
+},
+
+
+methods:{
+    addTask(){
+        //console.log('Funciona!!');
+
+        //this en este caso hace referencia a esta instancia Vue
+        // con el unshift le decimos a array tarea que coloque al inicio el parametro que pasamos que es nueva tarea
+        this.tareas.unshift({
+            nombre:this.nuevaTarea,
+            prioridad:false,
+            antiguedad:0,
+        });
+        // ahora clear la nueva tarea
+        this.nuevaTarea = null;
+
+
+
+    }
+},
+
+computed:{
+    mensajeAlreves(){
+        return this.mensaje.split('').reverse().join('');
+    },
+
+    tareaConPrioridad(){
+        return this.tareas.filter((tarea)=>tarea.prioridad);
+    },
+
+    tareaConAtiguedad(){
+        return this.tareas.sort((a,b)=>b.antiguedad - a.antiguedad);
+    }
+
+}
+
+
+
+
+}); */
+
+// video 7 filtros
+
+const vm = new Vue({
+    el:'main',
+    
+    
+
+    data:{
+        minimo: 1,
+        buscar:'',
+
+        peliculas:[
+        {nombre:'Ultimate', estreno:'2009', status:false,rank:10},
+        {nombre:'Pokemon', estreno:'1994', status:true,rank:1},
+        {nombre:'Resident Evil ', estreno:'2006', status:true,rank:7},
+        {nombre:'Efecto Mariposa', estreno:'2009', status:false,rank:2},
+    ],
+},
+
+
+computed: {
+
+        rankPelicula() {
+            return this.peliculas.filter((pelicula) => pelicula.rank >= this.minimo);
+    },
+
+        buscarPelicula() {
+            return this.peliculas.filter((pelicula) => pelicula.nombre.includes(this.buscar));
+        }
+
+}
 
 
 
